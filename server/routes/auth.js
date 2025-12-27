88536afe744d18ab52f0175046af9a6e6a367f84
VarozXYZ <alvaropiro@gmail.com>
2025-12-27 14:43:42 +0100

    feat: Add route for updating user plan

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const { register, login, getMe, updateProfile } = require('../controllers/authController')
+const { register, login, getMe, updateProfile, updatePlan } = require('../controllers/authController')
 const auth = require('../middleware/auth')
 
 router.post('/register', register)
 router.post('/login', login)
 router.get('/me', auth, getMe)
 router.put('/profile', auth, updateProfile)
+router.put('/plan', auth, updatePlan)
 
 module.exports = router
 
